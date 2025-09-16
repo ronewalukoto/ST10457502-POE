bae31b84e7013a4b84d0b926c281bbf3345c7991
ronewalukoto <st10457502@rcconnect.edu.za>
2025-09-16 11:47:54 +0200

    initial commit

---
+++ src/chatapppoe1/ChatAppPoe1.java
@@ -0,0 +1,16 @@
+
+package chatapppoe1;
+
+public class ChatAppPoe1 {
+
+ 
+    public static void main(String[] args) {
+      
+        String mess = "I have made it";
+        
+        System.out.println(mess);
+        
+        
+    }
+    
+}
